2e4d2b559f89cbf398091290817e9127084e876f
jafar <se.hasanjafar@gmail.com>
2024-05-25 10:34:58 +0530

    Constructor added

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -6,7 +6,15 @@ public class Message {
     private int id;
     private String content;
     private Date timestamp;
+    public Message(){
 
+    }
+
+    public Message(int id, String content) {
+        this.id = id;
+        this.content = content;
+        this.timestamp = new Date();
+    }
     public int getId() {
         return id;
     }
